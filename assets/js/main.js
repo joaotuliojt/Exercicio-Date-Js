72bd8721398a5fd056b21accc676846cddcb41de
joaotuliojt <joaotuliosoares@hotmail.com>
2021-07-14 00:40:15 -0300

    Segunda solução

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,95 +1,50 @@
 const date = new Date();
-const weekDay = date.getDay();
-const day = date.getDate();
-const mounth = date.getMonth();
-const year = date.getFullYear();
-const hours = date.getHours();
-const minutes = date.getMinutes();
 
-const txtDay = getTxtDay(weekDay);
-const txtMounth = getTxtMonth(mounth);
-const txtHours = formatHours(hours,minutes)
+const txtHours = formatHours()
+const day = getDate()
+setText(day,txtHours);
 
-setText(txtDay,day,txtMounth,year,txtHours);
 
-function formatHours(hours,minutes){
+function formatHours(){
+    const hours = zeroLeft(date.getHours());
+    const minutes = zeroLeft(date.getMinutes());
+ 
     return `${hours}:${minutes}`
 }
 
+function zeroLeft(num){
+    return num >= 10 ? num : `0${num}`
+}
+function getDate(){
+    let dayWeek = date.getDay();
+    let day = zeroLeft(date.getDate());
+    let mounth = date.getMonth();
+    let year = date.getFullYear();
+    
+    dayWeek = getTxtDay(dayWeek);
+    mounth = getTxtMonth(mounth);
+
+    return ` ${dayWeek}, ${day} de ${mounth} de ${year}`
+}
 function getTxtDay(day){
-    let txtDay;
-    switch(day){
-        case 0:
-            txtDay = 'Domingo'
-            return txtDay;
-        case 1:
-            txtDay = 'Segunda-Feira'
-            return txtDay;
-        case 2:
-            txtDay = 'Terça-Feira'
-            return txtDay;
-        case 3:
-            txtDay = 'Quarta-Feira'
-            return txtDay;
-        case 4:
-            txtDay = 'Quinta-Feira'
-            return txtDay;
-        case 5:
-            txtDay = 'Sexta-Feira';
-            return txtDay;
-        case 6:
-            txtDay = 'Sábado'
-            return txtDay;
-        default:
-            return txtDay = ' ';
-    }
+    let txtDay = ['Domingo','Segunda-Feira', 'Terça-Feira', 'Quarta-Feira', 'Quinta-Feira','Sexta-Feira','Sábado']
+    return txtDay[day];
 }
 
 function getTxtMonth(month){
-    let txtMonth;
-    switch(month){
-        case 0:
-            txtMonth = 'Janeiro'
-            return txtMonth;
-        case 1:
-            txtMonth = 'Fevereiro'
-            return txtMonth;
-        case 2:
-            txtMonth = 'Março'
-            return txtMonth;
-        case 3:
-            txtMonth = 'Abril'
-            return txtMonth;
-        case 4:
-            txtMonth = 'Maio'
-            return txtMonth;
-        case 5:
-            txtMonth = 'Junho'
-            return txtMonth;
-        case 6:
-            txtMonth = 'Julho'
-            return txtMonth;
-        case 7:
-            txtMonth = 'Agosto'
-            return txtMonth;
-        case 8:
-            txtMonth = 'Setembro'
-            return txtMonth;
-        case 9:
-            txtMonth = 'Outubro'
-            return txtMonth;
-        case 10:
-            txtMonth = 'Novembro'
-            return txtMonth;
-        case 11:
-            txtMonth = 'Dezembro'
-            return txtMonth;
-        default:
-            return txtMonth = ''
-    }
+    txtMonth =['Janeiro', 'Fevereiro', 'Março', 
+    'Abril', 'Maio', 'Junho', 'Julho', 'Agosto',
+    'Setembro', 'Outubro', 'Novembro', 'Dezembro']
+    return txtMonth[month]
 }
 
-function setText(txtDay,day,month,year,hour){
-    const section = document.querySelector('.container');
-    section.innerHTML = `<h1> ${txtDay}, ${day} de ${month} de ${year} ${hour} </h1>`
+function setText(day,hour){
+    const h1 = document.querySelector('.container h1');
+    h1.innerHTML = `${day} ${hour}`
 }
+
+
+
+const data = new Date();
+const h2 = document.querySelector('.container h2');
+h2.innerHTML = data.toLocaleString('pt-BR', {dateStyle: 'full',timeStyle:'short'})
